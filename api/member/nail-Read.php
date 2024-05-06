0be9fd5a63e39b978b8adf67fe32bae147af7fe7
soleylin <soley10160329@gmail.com>
2024-05-06 18:33:46 +0800

    add js

--- api/member/nail-Read.php
+++ api/member/nail-Read.php
@@ -1,5 +1,5 @@
 <?php
- header ("Access-Control-Allow-Origin: https://soleylin.github.io/SoleyStudio/nail.html");
+//  header ("Access-Control-Allow-Origin: https://soleylin.github.io/SoleyStudio/nail.html");
 $servername = "localhost";
 $username = "id22010393_soley";
 $password = "Fdio3_dine";
